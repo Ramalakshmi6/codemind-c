a3e6d06c8aac33902df31c9c51d4ccc71015889f
Rama lakshmi <98380738+Ramalakshmi6@users.noreply.github.com>
2022-04-22 10:34:42 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }     printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,17 @@
+
 #include<stdio.h>
 int main()
 {
-  int i,j,n;
-  scanf("%d",&n);
-  for(i=1;i<=n;i++)
-  {
-      for(j=1;j<=n;j++)
-      {
-          printf("%c ",i+64);
-      }
-      printf("
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+    printf("
 ");
-  } 
-  return 0;
+    }
+    return 0;
 }
